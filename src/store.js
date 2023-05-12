d94edeb90b4179407ae854d0d6a6d80c34a4b53c
Lizbeth Loginova <alice54prk@rambler.ru>
2023-05-12 21:30:53 +0600

    Task 2: unique code generation

--- src/store.js
+++ src/store.js
@@ -42,9 +42,14 @@ class Store {
    * Добавление новой записи
    */
   addItem() {
+    const lastElement = this.state.list[this.state.list.length - 1];
+    let code = 1;
+    if (lastElement) {
+      code = lastElement.code + 1;
+    }
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, {code, title: 'Новая запись'}]
     })
   };
 
